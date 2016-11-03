8c51cdb1a02516877b0d6c181ab6214b590e0f3e
Angelaaaaa <445769947@qq.com>
2016-11-03 22:31:48 +0000

    1

--- login/userinfo.py
+++ login/userinfo.py
@@ -4,12 +4,12 @@ from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
-class RegistrationForm(forms.Form):
+class Userinfo(forms.Form):
 
     username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
-    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
+    # email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
     password = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password (again)"))
+    # password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password (again)"))
 
     def clean_username(self):
         try:
--- login/views.py
+++ login/views.py
@@ -1,36 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 #views.py
 from login.forms import *
+from login.citation import *
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate
+from django.contrib.auth.models import UserManager
 # from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
+from login.models import *
+from login.userinfo import *
+
+
+def login(request):
+    if request.method == 'POST':
+            form = Userinfo(request.POST)
+
+
+            try:
+                p = User.objects.get(username=form.username)
+
+                if p == form.password:
+                     return home
+
+            except User.DoesNotExist:
+                raise forms.ValidationError("User not exist.")
+
+            #
+            # if user is not None:
+            #     return render(request, 'home.html', {'form': form})
+
+    return render(request, 'registration/login.html', {'form': form})
 
 # @csrf_protect
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(
+            # userManager =
+            # user = User(userManager, username=form.cleaned_data['username'],
+            #                                 email=form.cleaned_data['email'],
+            #                                password=form.cleaned_data['password'],)
+
+            user = User(
             username=form.cleaned_data['username'],
-            password=form.cleaned_data['password1'],
-            email=form.cleaned_data['email']
+            password=form.cleaned_data['password'],
+            email=form.cleaned_data['email'],
             )
-            return HttpResponseRedirect('/register/success/')
+            user.save()
+            return redirect('/register/success')
     else:
         form = RegistrationForm()
     variables = RequestContext(request, {
     'form': form
     })
 
-    return render_to_response(
-    'registration/register.html',
-    variables,
-    )
+    return render(request, 'registration/register.html', {'form': form})
 
 def register_success(request):
     return render_to_response(
@@ -43,7 +71,27 @@ def logout_page(request):
 
 @login_required
 def home(request):
-    return render_to_response(
-    'home.html',
-    { 'user': request.user }
-    )
+    # if request.method=='GET':
+    # obj = Citation.objects.all()
+
+    return render_to_response('home.html',{ 'obj': Citation.objects.all() } )
+    # if request.method =='POST':
+    #     return render_to_response('home.html',{'user':'whatever'})
+
+# @csrf_protect
+def submitcitation(request):
+    if request.method == 'POST':
+        form =  CitationForm(request.POST)
+        citation = Citation(
+                title=form.clean['title'],
+                notes=form.clean['notes'],
+                link=form.clean['link'],
+            )
+        citation.save()
+        return render_to_response('home.html', {'user': request.user})
+
+    else:
+        form = CitationForm()
+    variables = RequestContext(request, {'form': form})
+    # return render_to_response('AddCitation.html',variables)
+    return render(request, 'AddCitation.html', {'form': form})
